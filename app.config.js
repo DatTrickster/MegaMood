ef0ae59b1b2381d6c2aa3fac4696b2c5c47f4363
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2026-01-30 11:58:28 +0200

    Refactor app configuration by consolidating settings into app.config.js and removing app.json. Update Android and iOS configurations, including permissions and splash screen settings, for the MegaMood app.

--- app.config.js
+++ app.config.js
@@ -1,12 +1,43 @@
-const appJson = require('./app.json');
-
 module.exports = {
-  ...appJson,
   expo: {
-    ...appJson.expo,
-    plugins: [
-      ...(appJson.expo.plugins || []),
-      './plugins/withAndroidReleaseSigning.js',
-    ],
+    name: 'MegaMood',
+    slug: 'MegaMood',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/Icon.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription:
+          'MegaMood uses your location to show weather for your area when "Use precise location" is enabled in Settings.',
+      },
+      bundleIdentifier: 'com.anonymous.MegaMood',
+    },
+    android: {
+      permissions: [
+        'ACCESS_COARSE_LOCATION',
+        'ACCESS_FINE_LOCATION',
+        'ACCESS_COARSE_LOCATION',
+        'ACCESS_FINE_LOCATION',
+      ],
+      adaptiveIcon: {
+        foregroundImage: './assets/Icon.png',
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: 'com.anonymous.MegaMood',
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    plugins: ['./plugins/withAndroidReleaseSigning.js'],
   },
 };
